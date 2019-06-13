a3cb0f957d313561f8f81762304732bbd0697d78
rodriguesporan <rodriguesporan@gmail.com>
2019-06-13 19:05:05 -0300

    Finalizada a estilização do Feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -4,13 +4,62 @@ import api from '../services/api';
 import camera from '../assets/camera.png';
 import more from '../assets/more.png';
 import like from '../assets/like.png';
-import comment  from '../assets/send.png';
+import comment  from '../assets/comment.png';
 import send from '../assets/send.png';
 
 // import { Container } from './styles';
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
+  feedItem: {
+    marginTop: 20,
+  },
+  feedItemHeader: {
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  userInfo: {
+    
+  },
+  name: {
+    fontSize: 14,
+    color: '#000',
+  },
+  place: {
+    fontSize: 12,
+    color: '#666',
+    marginTop: 2,
+  },
+  feedImage: {
+    width: '100%',
+    height: 400,
+    marginVertical: 15,
+  },
+  feedItemFooter: {
+    paddingHorizontal: 15,
+  },
+  actions: {
+    flexDirection: 'row',
+  },
+  action: {
+    marginRight: 8,
+  },
+  likes: {
+    marginTop: 15,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  description: {
+    lineHeight: 18,
+    color: '#000',
+  },
+  hashtags: {
+    color: '#7159c1',
+  },
 });
 
 export default class Feed extends Component {
@@ -51,13 +100,13 @@ export default class Feed extends Component {
               <Image style={styles.feedImage} source={{ uri: `http://localhost:3000/files/${item.image}` }} />
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
                     <Image source={like} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
                     <Image source={comment} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => {}}>
                     <Image source={send} />
                   </TouchableOpacity>
                 </View>
