b1e3e7eec293733c72b2adbc3d70ddc644d399b3
rodriguesporan <rodriguesporan@gmail.com>
2019-06-13 17:03:36 -0300

    Retornando os dados da API

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
-import { View, Button, Image, TouchableOpacity } from 'react-native';
-
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import api from '../services/api';
 import camera from '../assets/camera.png';
 
 // import { Container } from './styles';
@@ -15,13 +14,30 @@ export default class Feed extends Component {
     ),
   });
 
+  state = {
+    feed: [],
+  }
+
+  async componentDidMount() {
+    // this.registerToSocket();
+    const response = await api.get('posts');
+    console.log(response.data);
+    this.setState({ feed: response.data });
+  }
+
+  renderItem({item}) {
+    return (
+      <Text>{item.author}</Text>
+    );
+  }
+
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View>
-        <Button
-          title="Go to New"
-          onPress={() => navigate('New')}
+        <FlatList
+          data={this.state.feed}
+          keyExtractor={post => post._id}
+          renderItem={this.renderItem}
         />
       </View>
     );
